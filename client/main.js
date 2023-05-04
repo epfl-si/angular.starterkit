a213772e3faacbb45e9fe679a22ff8182050f860
Dominique Quatravaux <dominique.quatravaux@epfl.ch>
2023-05-04 17:37:24 +0200

    [rewrite] client-side in “pure” JS
    - Leverages Webpack to import CSS and HTML files as strings
    - Poor man's templating with JavaScript string interpolation
    - Poor man's not-shadow DOM with `document.write()`

--- client/main.js
+++ client/main.js
@@ -1,22 +1,4 @@
-import { Template } from 'meteor/templating';
-import { ReactiveVar } from 'meteor/reactive-var';
+import html from './main.html';
+import css from './main.css';
 
-import './main.html';
-
-Template.hello.onCreated(function helloOnCreated() {
-  // counter starts at 0
-  this.counter = new ReactiveVar(0);
-});
-
-Template.hello.helpers({
-  counter() {
-    return Template.instance().counter.get();
-  },
-});
-
-Template.hello.events({
-  'click button'(event, instance) {
-    // increment the counter when button is clicked
-    instance.counter.set(instance.counter.get() + 1);
-  },
-});
+document.write(`${html}<style>${css}</style>`);
